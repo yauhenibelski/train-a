6b78d7ab63b6e7057f87201384b18f36d2e60a5f
Yauheni Belski <108809026+yauhenibelski@users.noreply.github.com>
2024-08-14 12:29:24 +0200

    Add train a backend (#2)
    * feat: attach mock backend functionality
    
    * chore: branch name validator

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -8,5 +9,6 @@ export const appConfig: ApplicationConfig = {
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
         provideAnimationsAsync(),
+        provideHttpClient(),
     ],
 };
