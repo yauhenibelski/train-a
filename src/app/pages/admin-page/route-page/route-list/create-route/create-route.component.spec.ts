ec0f79b608593cf70a19cd8ff748068123233389
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2024-09-18 19:57:03 +0200

    test: create-route.component

--- src/app/pages/admin-page/route-page/route-list/create-route/create-route.component.spec.ts
+++ src/app/pages/admin-page/route-page/route-list/create-route/create-route.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 import { CreateRouteComponent } from './create-route.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+import { MatDialogClose } from '@angular/material/dialog';
 
 describe('CreateRouteComponent', () => {
     let component: CreateRouteComponent;
@@ -8,7 +10,7 @@ describe('CreateRouteComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [CreateRouteComponent],
+            imports: [CreateRouteComponent, NoopAnimationsModule],
         }).compileComponents();
 
         fixture = TestBed.createComponent(CreateRouteComponent);
@@ -16,7 +18,15 @@ describe('CreateRouteComponent', () => {
         fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+    it('should save actual value', fakeAsync(() => {
+        const btn = fixture.debugElement.query(By.css('[test-id="save-btn"]'));
+        const matDialogClose = btn.injector.get(MatDialogClose);
+
+        component.carriagesStepper?.addOne(0, 'test');
+        component.stationStepper?.addOne(0, 1);
+
+        fixture.detectChanges();
+
+        expect(matDialogClose.dialogResult).toEqual({ carriages: ['test'], path: [1] });
+    }));
 });
