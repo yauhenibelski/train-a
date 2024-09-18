673e905789c1ac6c58c6ff152fe4282ad39c51df
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2024-09-18 12:00:42 +0200

    test: join.pipe

--- src/app/pages/admin-page/pipe/join/join.pipe.spec.ts
+++ src/app/pages/admin-page/pipe/join/join.pipe.spec.ts
@@ -1,8 +1,16 @@
 import { JoinPipe } from './join.pipe';
 
 describe('JoinPipe', () => {
-    it('create an instance', () => {
-        const pipe = new JoinPipe();
-        expect(pipe).toBeTruthy();
+    let pipe: JoinPipe;
+
+    beforeEach(() => {
+        pipe = new JoinPipe();
+    });
+
+    it('should return valid value', () => {
+        const { transform } = pipe;
+
+        expect(transform(123, '-')).toBeUndefined();
+        expect(transform([1, 2, 3], '-')).toBe('1-2-3');
     });
 });
